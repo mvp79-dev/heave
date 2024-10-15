0da14cb80ae5dd9bd27c163b76a5b6902791b3c9
mvp79-dev <toptalent48@gmail.com>
2024-10-15 16:30:29 +0300

    updates

--- app/Main/Section2.jsx
+++ app/Main/Section2.jsx
@@ -4,31 +4,67 @@ import gsap from "gsap";
 import SplitText from "gsap/src/SplitText";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import Image from "next/image";
+import { ChevronRight } from "lucide-react";
+import { motion } from "framer-motion";
+import Marquee from "react-fast-marquee";
 
 gsap.registerPlugin(SplitText, ScrollTrigger);
 
 export const Section2 = () => {
 
-  const titleRef = useRef()
-  const descriptionRef = useRef()
+  const img1Ref = useRef();
+  const img2Ref = useRef();
+  const img3Ref = useRef();
+  const img4Ref = useRef();
+  const img5Ref = useRef();
 
   useEffect(() => {
-    const split = new SplitText(titleRef.current, { type: "chars" });
-    gsap.fromTo(split.chars, { 'will-change': 'opacity, transform', opacity: 0, scale: 0.6, rotationZ: () => gsap.utils.random(-20,20)}, { ease: 'power4', opacity: 1, scale: 1, rotation: 0, stagger: 0.065, scrollTrigger: { trigger: titleRef.current, start: "top bottom", end: "bottom center", scrub: true } });
+    const imgs = [img1Ref.current, img2Ref.current, img3Ref.current, img4Ref.current,];
+    const duration = 1; // Duration of the animation
 
-    const titleSplitText = new SplitText(descriptionRef.current, { type: 'words' });
-    gsap.fromTo(titleSplitText.words, { opacity: 0, }, { opacity: 1, stagger: 0.01, scrollTrigger: { trigger: descriptionRef.current, start: "top bottom", end: "bottom center", scrub: true } })
-  }, [])
+    const crossfade = () => {
+      gsap.timeline({ onComplete: crossfade })
+        .to(imgs, { y: '-=80vh', duration, ease: "none" }) // Move images up
+        .to(imgs[0], { y: '+=240vh', duration: 0 }); // Move the first image to the end without delay
+
+      imgs.push(imgs.shift()); // Move the first image to the end of the array
+      console.log(imgs);
+    };
+
+    // Start the crossfade for the first time
+    crossfade();
+  }, []); 
 
   return (
     <section className="section two">
       <div className="section-border" />
       <div className="two-content">
         <div className="two-content-left">
-          <p className="description white" ref={descriptionRef} >We are passionate about elevating brands by leveraging viral marketing strategies that we have meticulously mastered over the years. Our approach combines innovative techniques and data-driven insights, ensuring your content resonates with audiences and engages them meaningfully. By creating campaigns that capture attention and drive results, we transform your brand’s visibility and impact. Let us partner with you to unlock your brand's full potential through effective and transformative marketing strategies.</p>
+          <div className="textbox">
+            <h1 className="subheadline white" >Making Short-Form Content That Captivates and Converts</h1>
+            <motion.button className="button">
+              <p className="description" >Let us tell you more</p>
+              <div className="button-iconbox">
+                <ChevronRight />
+              </div>
+            </motion.button>
+          </div>
         </div>
         <div className="two-content-right">
-          <h1 className="subheadline white" ref={titleRef} >About Us</h1>
+          <div className="two-marquee">
+            <div className="two-marquee-item" ref={img1Ref} >
+              <Image src="/images/heavephone.png" className="two-marquee-item-image" alt="alt" width={1000} height={1000} />
+            </div>
+            <div className="two-marquee-item" ref={img2Ref} >
+              <Image src="/images/heavelogo.png" className="two-marquee-item-image" alt="alt" width={1000} height={1000} />
+            </div>
+            <div className="two-marquee-item" ref={img3Ref} >
+              <Image src="/images/heavephone.png" className="two-marquee-item-image" alt="alt" width={1000} height={1000} />
+            </div>
+            <div className="two-marquee-item" ref={img4Ref} >
+              <Image src="/images/heavephone.png" className="two-marquee-item-image" alt="alt" width={1000} height={1000} />
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- app/Main/Section4.jsx
+++ app/Main/Section4.jsx
@@ -9,18 +9,9 @@ gsap.registerPlugin(SplitText, ScrollTrigger);
 
 export const Section4 = () => {
 
-    const videoRef = useRef()
-
-    useEffect(() => {
-        gsap.fromTo(videoRef.current, { yPercent: 7.5 }, { yPercent: -7.5, scrollTrigger: { trigger: ".four", start: "top bottom", end: "bottom top", scrub: true} })
-    }, [])
-
   return (
     <section className="section four">
         <div className="four-content">
-            <div className="four-content-frame">
-                <video src="/videos/test2.mp4" className="four-video" ref={videoRef} autoPlay="autoplay" muted playsInline="true" data-wf-ignore="true" preload="auto" loop />
-            </div>
         </div>
     </section>
   );
