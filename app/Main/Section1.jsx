77d3d32ca3eb27c1a99b623886a31c661b0700e1
mvp79-dev <toptalent48@gmail.com>
2024-11-05 21:54:19 +0200

    fix for 1

--- app/Main/Section1.jsx
+++ app/Main/Section1.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import SplitText from "gsap/src/SplitText";
 import ScrollTrigger from "gsap/ScrollTrigger";
