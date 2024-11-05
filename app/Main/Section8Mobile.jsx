d14ed9b0972a13219a1c1200fe8e03d41d6878af
mvp79-dev <toptalent48@gmail.com>
2024-11-05 22:22:42 +0200

    updates

--- app/Main/Section8Mobile.jsx
+++ app/Main/Section8Mobile.jsx
@@ -27,13 +27,13 @@ export const Section8Mobile = () => {
                 <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/DUNKIN.webp?v=1730742224537"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
             </div>
             <div className="eight-slider-item" >
-                <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/FASHIONNOVA.webp?v=1730742244549"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
+                <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/FASHIONNOVA2.webp?v=1730837608954"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
             </div>
             <div className="eight-slider-item" >
-                <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/HEXCLAD.webp?v=1730742257966"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
+                <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/HEXCLAD2.webp?v=1730837527138"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
             </div>
             <div className="eight-slider-item" >
-                <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/HINGELOGO.webp?v=1730742276687"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
+                <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/HINGELOGO2.webp?v=1730837375537"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
             </div>
             <div className="eight-slider-item" >
                 <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/JELLYBELLY.webp?v=1730742305727"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
@@ -45,22 +45,22 @@ export const Section8Mobile = () => {
                 <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/LIQUIDIV.webp?v=1730742318894"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
             </div>
             <div className="eight-slider-item" >
-                <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/POLYMARKET.webp?v=1730742377541"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
+                <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/POLYMARKET2.webp?v=1730837299614"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
             </div>
             <div className="eight-slider-item" >
-                <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/REEF.webp?v=1730742423440"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
+                <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/REEF2.webp?v=1730837209189"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
             </div>
             <div className="eight-slider-item" >
                 <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/SONIC.webp?v=1730742522716"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
             </div>
             <div className="eight-slider-item" >
-                <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/TACO%20BELL.svg?v=1730742500132"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
+                <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/TACOBELL.webp?v=1730837098525"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
             </div>
             <div className="eight-slider-item" >
                 <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/TEMU.webp?v=1730742469736"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
             </div>
             <div className="eight-slider-item" >
-                <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/THAIEXPRESS.webp?v=1730742438209"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
+                <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/thaiexpress.svg?v=1730837817703"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
             </div>
             <div className="eight-slider-item" >
                 <img src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/LYFT.webp?v=1730742358847"  className="eight-slider-item-image" width={500} height={500} alt="alt" />
