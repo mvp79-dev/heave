7bf932005ccdbfba3944eead8673e1184f6d23e5
mvp79-dev <toptalent48@gmail.com>
2024-11-10 21:47:09 +0200

    copyright text on footer

--- app/Main/SectionFooter.jsx
+++ app/Main/SectionFooter.jsx
@@ -6,19 +6,18 @@ gsap.registerPlugin(SplitText, ScrollTrigger, TextPlugin);
 
 export const SectionFooter = () => {
 
-  const titleRef = useRef()
+  // const titleRef = useRef()
 
-  useEffect(() => {
-    const split = new SplitText(titleRef.current, { type: "chars" });
-    gsap.fromTo(split.chars, { 'will-change': 'opacity, transform', opacity: 0, scale: 0.6, rotationZ: () => gsap.utils.random(-20,20)}, { ease: 'power4', opacity: 1, scale: 1, rotation: 0, stagger: 0.065, scrollTrigger: { trigger: titleRef.current, start: "top 95%" } });
-  }, [])
+  // useEffect(() => {
+  //   const split = new SplitText(titleRef.current, { type: "chars" });
+  //   gsap.fromTo(split.chars, { 'will-change': 'opacity, transform', opacity: 0, scale: 0.6, rotationZ: () => gsap.utils.random(-20,20)}, { ease: 'power4', opacity: 1, scale: 1, rotation: 0, stagger: 0.065, scrollTrigger: { trigger: titleRef.current, start: "top 95%" } });
+  // }, [])
 
   return (
     <section className="footer">
       <div className="footer-content">
-        <div className="footer-top">
-         <h1 className="footer-title white" ref={titleRef} >Heavecorp</h1>
-        </div>
+        <p className="description grey" >© 2024 Heave.</p>
+        <p className="description grey" >All rights reserved.</p>
       </div>
     </section>
   );
