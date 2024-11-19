cab6632d1505d7f7daa9c9ddb041a04e6c6f7cba
mvp79-dev <toptalent48@gmail.com>
2024-11-19 15:19:24 +0200

    updates

--- app/Main/Section2.jsx
+++ app/Main/Section2.jsx
@@ -5,9 +5,6 @@ import SplitText from "gsap/src/SplitText";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import Image from "next/image";
 import { ChevronRight } from "lucide-react";
-import { motion } from "framer-motion";
-import useEmblaCarousel from 'embla-carousel-react'
-import AutoScroll from 'embla-carousel-auto-scroll'
 
 gsap.registerPlugin(SplitText, ScrollTrigger);
 
@@ -18,22 +15,20 @@ export const Section2 = (options) => {
   const buttonRef2 = useRef()
   const sliderWrapperRef = useRef()
 
+  const contentRowRef1 = useRef()
+  const contentRowRef2 = useRef()
+  const contentRowRef3 = useRef()
+
   useEffect(() => {
     const titleSplitText = new SplitText(titleRef.current, { type: 'words' });
     gsap.fromTo(titleSplitText.words, { opacity: 0 }, { opacity: 1, stagger: 0.05, duration: 1, scrollTrigger: { trigger: titleRef.current, start: "top 95%" } })
     gsap.fromTo(buttonRef.current, { opacity: 0 }, { opacity: 1, duration: 1, scrollTrigger: { trigger: buttonRef.current, start: "top 95%" } })
     gsap.fromTo(buttonRef2.current, { opacity: 0 }, { delay: 0.25, opacity: 1, duration: 1, scrollTrigger: { trigger: buttonRef.current, start: "top 95%" } })
-    if (window.innerWidth > 768) {
-      gsap.fromTo(sliderWrapperRef.current, { rotate: "27.5deg", translateY: "-30vh", translateX: "2.5vw" }, { rotate: "0deg", translateY: "0vh", translateX: "0vw", scrollTrigger: { trigger: ".two-content-right", start: "top bottom", end: "bottom top", scrub: true } })
-    }
+    gsap.fromTo(sliderWrapperRef.current, { rotate: "27.5deg", translateY: "-30vh", translateX: "2.5vw" }, { rotate: "0deg", translateY: "0vh", translateX: "0vw", scrollTrigger: { trigger: ".two-content-right", start: "top bottom", end: "bottom top", scrub: true } })
+    gsap.fromTo(contentRowRef1.current, { translateY: "40vh" }, { translateY: "-40vh", scrollTrigger: { trigger: ".two-content-right", start: "top bottom", end: "bottom top", scrub: true } })
+    gsap.fromTo(contentRowRef2.current, { translateY: "-40vh" }, { translateY: "40vh", scrollTrigger: { trigger: ".two-content-right", start: "top bottom", end: "bottom top", scrub: true } })
+    gsap.fromTo(contentRowRef3.current, { translateY: "40vh" }, { translateY: "-40vh", scrollTrigger: { trigger: ".two-content-right", start: "top bottom", end: "bottom top", scrub: true } })
   }, [])
-
-  // CAROUSEL
-  const [emblaRef] = useEmblaCarousel({axis: "y", loop: true, watchDrag: false}, [AutoScroll({speed: 2 })] )
-
-  const [emblaRef2] = useEmblaCarousel({axis: "y", loop: true, watchDrag: false}, [AutoScroll({speed: -2 })] )
-
-  const [emblaRef3] = useEmblaCarousel({axis: "y", loop: true, watchDrag: false}, [AutoScroll({speed: 2 })] )
 
   const scrollToSectionTop = (sectionId) => {
     const section = document.getElementById(sectionId);
@@ -62,8 +57,8 @@ export const Section2 = (options) => {
           <div className="top-fade" />
           <div className="bottom-fade" />
           <div className="two-content-right-content" ref={sliderWrapperRef} >
-            <div className="two-carousel" ref={emblaRef} >
-              <div className="two-row">
+            <div className="two-content-right-content-row" >
+              <div className="two-content-right-content-row-content" ref={contentRowRef1} >
                 <div className="two-item" >
                   <video src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/heavevideo1.mp4?v=1730721648346" className="two-item-image" autoPlay="autoplay" muted playsInline={true} loop />
                 </div>
@@ -76,22 +71,10 @@ export const Section2 = (options) => {
                 <div className="two-item" >
                   <video src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/heavevideo1.mp4?v=1730721648346" className="two-item-image" autoPlay="autoplay" muted playsInline={true} loop />
                 </div>
-                <div className="two-item" >
-                  <video src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/heavevideo2.mp4?v=1730721652829" className="two-item-image" autoPlay="autoplay" muted playsInline={true} loop />
-                </div>
-                <div className="two-item" >
-                  <video src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/heavevideo3.mp4?v=1730721657920" className="two-item-image" autoPlay="autoplay" muted playsInline={true} loop />
-                </div>
-                {/* <div className="two-item" >
-                  <video src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/heavevideo4.mp4?v=1730721661781" className="two-item-image" autoPlay="autoplay" muted playsInline={true} loop />
-                </div> */}
-                {/* <div className="two-item" >
-                  <video src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/heavevideo5.mp4?v=1730721665211" className="two-item-image" autoPlay="autoplay" muted playsInline={true} loop />
-                </div> */}
               </div>
             </div>
-            <div className="two-carousel" ref={emblaRef2} >
-              <div className="two-row">
+            <div className="two-content-right-content-row">
+              <div className="two-content-right-content-row-content" ref={contentRowRef2} >
                 <div className="two-item" >
                   <video src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/heavevideo4.mp4?v=1730721661781" className="two-item-image" autoPlay="autoplay" muted playsInline={true} loop />
                 </div>
@@ -104,22 +87,10 @@ export const Section2 = (options) => {
                 <div className="two-item" >
                   <video src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/heavevideo4.mp4?v=1730721661781" className="two-item-image" autoPlay="autoplay" muted playsInline={true} loop />
                 </div>
-                <div className="two-item" >
-                  <video src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/heavevideo5.mp4?v=1730721665211" className="two-item-image" autoPlay="autoplay" muted playsInline={true} loop />
-                </div>
-                <div className="two-item" >
-                  <video src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/heavevideo6.mp4?v=1730721670541" className="two-item-image" autoPlay="autoplay" muted playsInline={true} loop />
-                </div>
-                {/* <div className="two-item" >
-                  <video src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/heavevideo8.mp4?v=1730721676866" className="two-item-image" autoPlay="autoplay" muted playsInline={true} loop />
-                </div> */}
-                {/* <div className="two-item" >
-                  <video src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/heavevideo7.mp4?v=1730721673586" className="two-item-image" autoPlay="autoplay" muted playsInline={true} loop />
-                </div> */}
               </div>
             </div>
-            <div className="two-carousel two-carousel-hidden" ref={emblaRef3} >
-              <div className="two-row">
+            <div className="two-content-right-content-row">
+              <div className="two-content-right-content-row-content" ref={contentRowRef3} >
                 <div className="two-item" >
                   <video src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/heavevideo7.mp4?v=1730721673586" className="two-item-image" autoPlay="autoplay" muted playsInline={true} loop />
                 </div>
@@ -132,18 +103,6 @@ export const Section2 = (options) => {
                 <div className="two-item" >
                   <video src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/heavevideo7.mp4?v=1730721673586" className="two-item-image" autoPlay="autoplay" muted playsInline={true} loop />
                 </div>
-                <div className="two-item" >
-                  <video src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/heavevideo8.mp4?v=1730721676866" className="two-item-image" autoPlay="autoplay" muted playsInline={true} loop />
-                </div>
-                <div className="two-item" >
-                  <video src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/heavevideo9.mp4?v=1730721682141" className="two-item-image" autoPlay="autoplay" muted playsInline={true} loop />
-                </div>
-                {/* <div className="two-item" >
-                  <video src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/heavevideo9.mp4?v=1730721682141" className="two-item-image" autoPlay="autoplay" muted playsInline={true} loop />
-                </div> */}
-                {/* <div className="two-item" >
-                  <video src="https://cdn.glitch.global/fb77b0c4-3062-4970-a03b-49138657d8c7/heavevideo3.mp4?v=1730721657920" className="two-item-image" autoPlay="autoplay" muted playsInline={true} loop />
-                </div> */}
               </div>
             </div>
           </div>
