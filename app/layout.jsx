cb0132097c0ddd8ad98f91bfebc1f462af8a760c
mvp79-dev <toptalent48@gmail.com>
2024-11-29 12:11:52 +0200

    updates

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import { Navigation } from "./Navigation";
 export const metadata = {
   title: "Heave",
   description: "We elevate brands through viral marketing strategies we’ve mastered in the past.",
+  viewport: "width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no",
 };
 
 export default function RootLayout({ children }) {
